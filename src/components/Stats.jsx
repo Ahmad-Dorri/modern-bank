354aafcecf0e7556f61fe9e910228b90ac1b79eb
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-04-24 16:55:44 +0430

    stats section added

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,23 @@
+import { stats } from '../constants';
+import styles from '../style';
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className={`flex flex-1 flex-row  items-center sm:justify-start justify-center m-3  `}>
+          <h4 className="text-white  sm:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] font-semibold font-poppins">
+            {stat.value}
+          </h4>
+          <p className="text-gradient  sm:text-[20px] text-[15px] xs:leading-[25px] leading-[21px] font-normal font-poppins ml-3 uppercase truncate">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
